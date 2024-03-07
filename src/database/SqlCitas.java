1c44f90627818b4514c2a6cdf4215d97f614f397
AndresBarraganM <132424776+AndresBarraganM@users.noreply.github.com>
2024-03-07 10:10:48 -0800

    SqlCitas actualizado
    Falta por agregar metodos especificos de citas

--- src/database/SqlCitas.java
+++ src/database/SqlCitas.java
@@ -8,55 +8,219 @@ import cites.Cita;
 import cites.Coordinador;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
 
 
 /**
  *
  * @author Usuario
  */
-/*
-public class SqlCitas extends SqlConector {
-    public void crearCita(Cita cita){ }
 
-
+public class SqlCitas extends SqlConector { //TODO Por probar
     
-    public Cita[] consultarCitasPorAlumno(int idAlumno){
-        Cita citaAlumno = new Cita();
-        String sqlSelect = "SELECT * FROM Alumnos WHERE correo = (?) AND contrasenia = (?)";
-
+    private DefaultTableModel DT;
+    
+    private DefaultTableModel setTable(){
+        DT = new DefaultTableModel();
+        DT.addColumn("idCitas");
+        DT.addColumn("citadorID");
+        DT.addColumn("coordinadorID");
+        DT.addColumn("hora");
+        DT.addColumn("motivo");
+        DT.addColumn("anio");
+        DT.addColumn("estado");
+        DT.addColumn("dia");
+        DT.addColumn("mes");
+        return DT;
+    }
+    
+    public void crearCita(Cita cita){ 
+        //Obtener datos
+        //String id = cita.getId_cita();
+        int anio = cita.getAnio();
+        int mes = cita.getMes();
+        int dia = cita.getDia();
         
-        try {
-            PreparedStatement PS = this.getConnection().prepareStatement(sqlSelect);
-            RS = PS.executeQuery();
+        int hora = cita.getHora();
+        
+        String citadorId = cita.getCitadorId();
+        String citadorDocente = cita.getCitadorDocente();
+        String motivo = cita.getMotivo();
+        String estado = cita.getEstado();
+        
+        String sqlInsert= "INSERT INTO citas"
+                + "(citadorID, coordinadorID, hora, motivo, anio, estado, dia, mes)"
+                + "values (?,?,?,?,?,?,?,?)";
+        
+        
+        try{
+            this.conectar();
+                    this.DT = setTable();
+            PreparedStatement PS = this.getConnection().prepareStatement(sqlInsert);
             
-            // Agregado para la base de datos 19/02/2024
-            citaAlumno.setId_cita(RS.getString(1)); 
-            citaAlumno.setFecha( RS.getString(2));
-            citaAlumno.setHora( Integer.parseInt(RS.getString(3)));
-            citaAlumno.setCitadorId(RS.getString(4));
-            citaAlumno.setCitadorDocente(RS.getString(5));
-            citaAlumno.setMotivo(RS.getString(4));
-            citaAlumno.setEstado(RS.getString(5));
+            PS.setString(1, citadorId);
+            PS.setString(2, citadorDocente);
+            PS.setInt(3,hora);
+            PS.setString(4,motivo);
+            PS.setInt(5,anio);
+            PS.setString(6, estado);
+            PS.setInt(7, dia);
+            PS.setInt(8,mes);
             
-            //Desconosco si esto se rompera por que el metodo esta dise;ado para agregar a listas
+            PS.executeUpdate();
+            
         } catch(Exception e){
-            System.out.println("Error al modificar datos de alumno: "+e.getMessage());
-            
+            System.err.println("Error al crear cita en BD: "+ e.getMessage());
         } finally{
-            this.desconectar();
-            this.close();
-            return alumno;
+                this.desconectar();
         }
     }
-        return 
-    }
+
+
     
-    
-    public Cita[] consultarCitasPorCoordinador(int idCoordinador){
+    public DefaultTableModel consultarCitasPorAlumno(String idAlumno){
+        //Codigo de: https://www.youtube.com/watch?v=dSn4ZORiqpY
+        Cita citaAlumno = new Cita();
+        String sqlSelect = "SELECT * FROM Citas WHERE citadorID = (?)";
         
-        return
-    }
+        try{
+            this.conectar();
+            PreparedStatement PS = this.getConnection().prepareStatement(sqlSelect);
+            PS.setString(1,idAlumno);
+            this.DT = setTable();
+            
+            RS = PS.executeQuery();
+            
+            if(!RS.isBeforeFirst()){
+                throw new Exception("No cita encontrada");
+            }
+            
+            Object[] fila = new Object[9];
+            while(RS.next()){
+                fila[0] = RS.getInt(1);
+                fila[1] = RS.getInt(2);
+                fila[2] = RS.getInt(3);
+                fila[3] = RS.getInt(4);
+                fila[4] = RS.getString(5);
+                fila[5] = RS.getInt(6);
+                fila[6] = RS.getString(7);
+                fila[7] = RS.getInt(8);
+                fila[8] = RS.getInt(9);
+                DT.addRow(fila);
+            }
+        } catch(Exception e){
+            if (e.getMessage().equals("No cita encontrada")){
+                System.out.println("No cita encontrada ocn estos parametros");
+            } else{
+                System.err.println("Error al consultar citas: "+e);
+            } 
+        }finally{
+            this.close();               
+        }
+        return DT;
+    } 
     
-    public void cambiarEstadoCita(char estadoNuevo){}
+     public DefaultTableModel consultarCitasPorCoordinador(String idCoordinador){
+        //Codigo de: https://www.youtube.com/watch?v=dSn4ZORiqpY
+        Cita citaCoord= new Cita();
+        String sqlSelect = "SELECT * FROM Citas WHERE coordinadorID = (?)";
+        
+        try{
+            this.conectar();
+            PreparedStatement PS = this.getConnection().prepareStatement(sqlSelect);
+            PS.setString(1,idCoordinador);
+            this.DT = setTable();
+            
+            RS = PS.executeQuery();
+            
+            if(!RS.isBeforeFirst()){
+                throw new Exception("No cita encontrada");
+            }
+            
+            Object[] fila = new Object[9];
+            while(RS.next()){
+                fila[0] = RS.getInt(1);
+                fila[1] = RS.getInt(2);
+                fila[2] = RS.getInt(3);
+                fila[3] = RS.getInt(4);
+                fila[4] = RS.getString(5);
+                fila[5] = RS.getInt(6);
+                fila[6] = RS.getString(7);
+                fila[7] = RS.getInt(8);
+                fila[8] = RS.getInt(9);
+                DT.addRow(fila);
+            }
+        } catch(Exception e){
+            if (e.getMessage().equals("No cita encontrada")){
+                System.out.println("No cita encontrada ocn estos parametros");
+            } else{
+                System.err.println("Error al consultar citas: "+e);
+            } 
+        }finally{
+            this.close();               
+        }
+        return DT;
+    } 
+     
+    public DefaultTableModel consultarCitasPorCoordinadorAceptadas(String idCoordinador, char estado){
+        //Codigo de: https://www.youtube.com/watch?v=dSn4ZORiqpY
+        Cita citaCoord= new Cita();
+        String sqlSelect = "SELECT * FROM Citas WHERE coordinadorID = (?) AND estado = (?)";
+        
+        try{
+            this.conectar();
+            PreparedStatement PS = this.getConnection().prepareStatement(sqlSelect);
+            PS.setString(1,idCoordinador);
+            PS.setString(2,String.valueOf(estado));
+            this.DT = setTable();
+            
+            RS = PS.executeQuery();
+            
+            if(!RS.isBeforeFirst()){
+                throw new Exception("No cita encontrada");
+            }
+            
+            Object[] fila = new Object[9];
+            while(RS.next()){
+                fila[0] = RS.getInt(1);
+                fila[1] = RS.getInt(2);
+                fila[2] = RS.getInt(3);
+                fila[3] = RS.getInt(4);
+                fila[4] = RS.getString(5);
+                fila[5] = RS.getInt(6);
+                fila[6] = RS.getString(7);
+                fila[7] = RS.getInt(8);
+                fila[8] = RS.getInt(9);
+                DT.addRow(fila);
+            }
+        } catch(Exception e){
+            if (e.getMessage().equals("No cita encontrada")){
+                System.out.println("No cita encontrada ocn estos parametros");
+            } else{
+                System.err.println("Error al consultar citas: "+e);
+            } 
+        }finally{
+            this.close();               
+        }
+        return DT;
+    } 
+     
+    public void cambiarEstadoCita(String idCita, char estadoNuevo){
+        //TODO Revisar que si esten asi la tabla citas
+        String sqlUpdate= "UPDATE Citas SET estado = (?) WHERE idCitas = (?)";
+        
+        try{
+            this.conectar();
+            PreparedStatement PS = this.getConnection().prepareStatement(sqlUpdate);
+            PS.setString(1, String.valueOf(estadoNuevo));
+            PS.setString(2, idCita);
+            
+            PS.execute();
+        } catch (SQLException e){
+            System.out.println("Error al modificar registro de cita: "+e.getMessage());
+        } finally {
+            this.desconectar();
+            //this.close();
+        }
+    }
 }
-*/
