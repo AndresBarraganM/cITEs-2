d99ee1e770821e7420418a203cace23452c299d9
AndresBarraganM <132424776+AndresBarraganM@users.noreply.github.com>
2024-03-10 18:46:11 -0700

    testBD actualizado
    Se le agrego metodo para probar citas

--- testBD.java
+++ testBD.java
@@ -8,25 +8,25 @@
  * @author Usuario
  */
 
-
+import cites.Cita;
 import cites.Alumno;
 import database.SqlAlumno;
-
+import database.SqlCitas;
+import javax.swing.table.DefaultTableModel;
 /*
 Este es un archivo hecho para probar la base de datos, sientanse
 Sientanse libres de cambiar los valores si quieren probar algo en especifico
 */
 public class testBD {
   public static void main(String[] args){
-    //crearAlumno();
-    //modificarAlumno();
-    consultarAlumno();
+    
+    probarCitas();
     
   }
   
   
   public static void crearAlumno(){
-     Alumno alumno = new Alumno();
+    Alumno alumno = new Alumno();
     alumno.setContrasena("myPassword");
     alumno.setCorreoInstitucional("al22760555@ite.edu.mx");
     //alumno.setId("11111"); Autocreada
@@ -44,13 +44,14 @@ public class testBD {
       SqlAlumno conectBD = new SqlAlumno();
       Alumno alumno = conectBD.consultarAlumno(contrasena, correo);
       
-      System.out.println(alumno);
+      System.out.println(alumno.toString());
   }
   
   public static void modificarAlumno(){
      //La id que ocupo
     Alumno alumno = new Alumno();
-    alumno.setId("0000000003");
+    
+    alumno.setId("1");
     
     
      //Lo que quiero cambiar
@@ -61,5 +62,42 @@ public class testBD {
     
     SqlAlumno coneccionBD = new SqlAlumno();
     coneccionBD.modificarAlumno(alumno);
+  }
+  
+  public static void probarCitas(){
+      SqlCitas dbCon = new SqlCitas();
+      
+        //crear objeto citas
+      Cita cita = new Cita();
+      cita.setAnio(2024);
+      cita.setMes(3);
+      cita.setDia(27);
+      cita.setHora(1040);
+      
+      cita.setCitadorDocente("000003");
+      cita.setCitadorId("000006");
+      
+      cita.setMotivo("preguntas");
+      cita.setEstado("p");
+      
+      
+      
+      
+      //guardar una cita
+      //dbCon.crearCita(cita);
+      
+      //modificar la cita
+      //dbCon.cambiarEstadoCita("1", 'd');
+      
+      //ver todass las citas
+      DefaultTableModel DT = new DefaultTableModel();
+      
+      DT = dbCon.consultarCitasPorAlumno("1");
+      System.out.println(DT);
+      
+      DT = dbCon.consultarCitasPorCoordinador("1");
+      
+      System.out.println(DT);
+      
   }
 }
